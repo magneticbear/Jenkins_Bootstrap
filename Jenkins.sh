b3b604cd6ce6170b351fe0b1d52fa7b27a8fb79e
JP Simard <jp@jpsim.com>
2013-02-21 00:44:08 -0500

    Cleanup and simplification

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,57 +1,73 @@
 #!/bin/bash
 
+# Xcode
+
 echo "Installing Xcode Command Line Tools"
 hdiutil mount -noverify xcode_cli.dmg
 echo $1 | sudo -S installer -package "/Volumes/Command Line Tools (Mountain Lion)/Command Line Tools (Mountain Lion).mpkg" -target /
 umount "/Volumes/Command Line Tools (Mountain Lion)"
 
-echo "Installing Xcode 4.6"
+echo "Installing Xcode"
 hdiutil mount -noverify xcode.dmg
 echo $1 | sudo -S cp -R "/Volumes/Xcode/Xcode.app" /Applications
 umount "/Volumes/Xcode"
 
+echo "Enabling Mac Developer Mode"
 echo $1 | sudo -S /usr/sbin/DevToolsSecurity -enable
 echo $1 | /usr/sbin/dseditgroup -o edit -t group -a staff _developer
 
 expect "accept_xcode_license.exp" $1
 
-echo "Installing Homebrew"
-expect "accept_homebrew.exp" $1 "go.rb"
+# Homebrew, git, cocoapods, node
 
-echo "Installing JDK"
-hdiutil mount -noverify jdk-7u15-macosx-x64.dmg
-echo $1 | sudo -S installer -package "/Volumes/JDK 7 Update 15/JDK 7 Update 15.pkg" -target /
-umount "/Volumes/JDK 7 Update 15"
-echo "finished installing the JDK"
+echo "Installing Homebrew"
+curl -O https://raw.github.com/mxcl/homebrew/go
+expect "accept_homebrew.exp" $1 "go"
 
 echo "Installing git"
 brew install git
-
-echo "Installing Eclipse 4.2.1"
-tar -zxvf eclipse-SDK-4.2.1-macosx-cocoa-x86_64.tar
-mv Eclipse /Applications/Eclipse
-
-echo "Installing oh my zsh"
-echo $1 | sudo -S sh install.sh
-echo $1 | sudo -S chsh -s /bin/zsh
-
-echo "Downloading Latest Version of Jenkins"
-cd ~/Downloads
-curl -L -O http://mirrors.jenkins-ci.org/osx/latest
-mv latest jenkins.pkg
-echo "Installing Jenkins"
-echo $1 | sudo -S  installer -pkg jenkins.pkg -target /
 
 echo $1 | sudo -S gem update --system
 echo "Installing cocoapods"
 echo $1 | sudo -S gem install cocoapods
 pod setup
 
-echo "Downloading Jenkins CLI"
-curl -L -O http://localhost:8080/jnlpJars/jenkins-cli.jar
-
 echo "Installing node"
 brew install node
+
+# Eclipse
+
+echo "Installing Eclipse"
+tar -zxf eclipse.tar
+mv Eclipse /Applications/Eclipse
+
+# Java
+
+echo "Installing JDK"
+hdiutil mount -noverify jdk.dmg
+echo $1 | sudo -S installer -package "/Volumes/JDK 7 Update 15/JDK 7 Update 15.pkg" -target /
+umount "/Volumes/JDK 7 Update 15"
+
+echo "Installing Java 6"
+hdiutil mount -noverify jre6.dmg
+echo $1 | sudo -S installer -package "/Volumes/Java for OS X 2013-001/JavaForOSX.pkg" -target /
+umount "/Volumes/Java for OS X 2013-001"
+
+echo "Installing Java 7"
+hdiutil mount -noverify jre7.dmg
+echo $1 | sudo -S installer -package "/Volumes/Java 7 Update 15/Java 7 Update 15.pkg" -target /
+umount "/Volumes/Java 7 Update 15"
+
+# Jenkins + Tools
+
+echo "Downloading Latest Version of Jenkins"
+cd ~/Downloads
+curl -L -o jenkins.pkg http://mirrors.jenkins-ci.org/osx/latest
+echo "Installing Jenkins"
+echo $1 | sudo -S  installer -pkg jenkins.pkg -target /
+
+echo "Downloading Jenkins CLI"
+curl -L -O http://localhost:8080/jnlpJars/jenkins-cli.jar
 
 echo "Installing Jenkins Plugins"
 echo "Installing xcode Plugin"
